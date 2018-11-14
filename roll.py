dfb72d8b90e64d685750f11cce893c445fe0daf2
deadbananas <45050790+deadbananas@users.noreply.github.com>
2018-11-14 18:08:08 -0500

    Update roll.py

--- roll.py
+++ roll.py
@@ -28,4 +28,4 @@ async def on_message(message):
         randomlist = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20',]
         await client.send_message(message.channel,(random.choice(randomlist)))
 
-client.run('Bot_token')
+client.run('NTEyMDk2ODIzNzIxMzI4NjUw.Ds4t0g.6QwYHlZ9EGKAq8xN61WNGExyyHE')

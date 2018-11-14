3909a095efa6aa37edd092b836632e7cee73f31b
deadbananas <45050790+deadbananas@users.noreply.github.com>
2018-11-14 18:20:52 -0500

    Update roll.py

--- roll.py
+++ roll.py
@@ -25,7 +25,7 @@ async def on_message(message):
     if ('frick') in message.content:
        await client.delete_message(message)
     if message.content.startswith('!d20'):
-        randomlist = ['1','2','3','4','5','6','7','8'.'9'.'10'.'11'.'12','13','14','15','16','17','18','19','20',]
+        randomlist = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20',]
         await client.send_message(message.channel,(random.choice(randomlist)))
 
 
